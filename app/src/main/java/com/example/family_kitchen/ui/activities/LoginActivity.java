bd039302f3d80c1d23804621518b8ccac004da2a
Rouzen <zayode_142015@hotmail.com>
2023-01-01 02:31:41 +0300

    UI design

--- app/src/main/java/com/example/family_kitchen/ui/activities/LoginActivity.java
+++ app/src/main/java/com/example/family_kitchen/ui/activities/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.family_kitchen;
+package com.example.family_kitchen.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.family_kitchen.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/example/family_kitchen/ui/activities/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.family_kitchen.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.family_kitchen.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
